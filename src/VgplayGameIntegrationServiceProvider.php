4d2146cdbbce7e2965a9302aa10cf3a6161b27f1
dungnt2 <dungnt2.ctv@gmail.com>
2022-03-03 10:29:41 +0700

    update VgplayGameIntegrationServiceProvider.php

--- src/VgplayGameIntegrationServiceProvider.php
+++ src/VgplayGameIntegrationServiceProvider.php
@@ -6,8 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class VgplayGameIntegrationServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/configs/games.php', 'games');
+    }
+
+    public function boot()
+    {
+        # Code
     }
 }
